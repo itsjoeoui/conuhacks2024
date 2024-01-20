c1dbbcc0488e6b5f8a3e1ae4becf5938d9d70afb
alexandrepayumo <alexpayumo@hotmail.com>
2024-01-20 18:04:28 -0500

    temp commit

--- src/server/api/routers/message.ts
+++ src/server/api/routers/message.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-
+//test
 export const messageRouter = createTRPCRouter({
   getMessagesByExchange: publicProcedure.query(async ({ input, ctx }) => {
     return await ctx.db.query.message.findMany({
