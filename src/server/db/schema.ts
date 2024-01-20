be77c7166bce1ac858941bf1944a4d3a82110938
Joey Yu <joey@jyu.dev>
2024-01-20 12:54:31 -0500

    chore: setup enum

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -5,10 +5,21 @@ export const pgTable = pgTableCreator((name) => `conuhacks2024_${name}`);
 export const message = pgTable("message", {
   timeStamp: timestamp("time_stamp").notNull(),
   timeStampEpoch: timestamp("time_stamp_epoch").notNull(),
-  direction: text("direction").notNull(),
+  direction: text("direction", {
+    enum: ["ExchangeToNBF", "NBFToExchange"],
+  }).notNull(),
   orderId: text("order_id").notNull(),
-  messageType: text("message_type").notNull(),
+  messageType: text("message_type", {
+    enum: [
+      "CancelRequest",
+      "NewOrderRequest",
+      "NewOrderAcknowledged",
+      "CancelAcknowledged",
+    ],
+  }).notNull(),
   symbol: text("symbol").notNull(),
   orderPrice: bigint("order_price", { mode: "number" }).notNull(),
-  exchange: text("exchange").notNull(),
+  exchange: text("exchange", {
+    enum: ["Exchange_1", "Exchange_2", "Exchange_3"],
+  }).notNull(),
 });
