96e518a1ce3bcb0c0a4f37193682fa1cbe464c40
Joey Yu <joey@jyu.dev>
2024-01-20 12:50:11 -0500

    feat: message tabe

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,3 +1,14 @@
-import { pgTableCreator } from "drizzle-orm/pg-core";
+import { bigint, pgTableCreator, text, timestamp } from "drizzle-orm/pg-core";
 
 export const pgTable = pgTableCreator((name) => `conuhacks2024_${name}`);
+
+export const message = pgTable("message", {
+  timeStamp: timestamp("time_stamp").notNull(),
+  timeStampEpoch: timestamp("time_stamp_epoch").notNull(),
+  direction: text("direction").notNull(),
+  orderId: text("order_id").notNull(),
+  messageType: text("message_type").notNull(),
+  symbol: text("symbol").notNull(),
+  orderPrice: bigint("order_price", { mode: "number" }).notNull(),
+  exchange: text("exchange").notNull(),
+});
