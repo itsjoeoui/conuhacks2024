a79fd67068950809b7f009e23369297c01652ac1
Joey Yu <joey@jyu.dev>
2024-01-20 22:08:45 -0500

    chore: new icons

--- src/components/tabs/overview-tab.tsx
+++ src/components/tabs/overview-tab.tsx
@@ -9,6 +9,12 @@ import { TabsContent } from "~/components/ui/tabs";
 import { RecentTrades } from "../recent-trades";
 import { type SelectMessage } from "~/types/message";
 import PlotStreamingComponent from "~/components/plotstreaming";
+import {
+  CheckCircleIcon,
+  DollarSign,
+  ShieldAlert,
+  XCircle,
+} from "lucide-react";
 
 type Props = {
   messages: SelectMessage[];
@@ -27,18 +33,7 @@ const OverviewTab = ({ messages, stats }: Props) => {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Total Volumn</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-            </svg>
+            <DollarSign />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">{stats.total_volumn}</div>
@@ -52,20 +47,7 @@ const OverviewTab = ({ messages, stats }: Props) => {
             <CardTitle className="text-sm font-medium">
               Total Completed
             </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-              <circle cx="9" cy="7" r="4" />
-              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-            </svg>
+            <CheckCircleIcon />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">{stats.total_completed}</div>
@@ -79,19 +61,7 @@ const OverviewTab = ({ messages, stats }: Props) => {
             <CardTitle className="text-sm font-medium">
               Total Canceled
             </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <rect width="20" height="14" x="2" y="5" rx="2" />
-              <path d="M2 10h20" />
-            </svg>
+            <XCircle />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">{stats.total_canceled}</div>
@@ -105,18 +75,7 @@ const OverviewTab = ({ messages, stats }: Props) => {
             <CardTitle className="text-sm font-medium">
               Total Rejected
             </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-            </svg>
+            <ShieldAlert />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">{stats.total_rejected}</div>
