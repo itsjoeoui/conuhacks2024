beb05ccd0ae5c874dc29488620373dcbe587c87c
Joey Yu <joey@jyu.dev>
2024-01-21 10:18:17 -0500

    feat: better chatbot

--- src/components/tabs/analytics-tab.tsx
+++ src/components/tabs/analytics-tab.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { TabsContent } from "../ui/tabs";
-import SQLQuery from "../sql-query";
+// import SQLQuery from "../sql-query";
 import Chatbot from "../chatbot";
 
 const AnalyticsTab = () => {
   return (
     <TabsContent value="analytics" className="space-y-4">
       <Chatbot />
-      <SQLQuery />
+      {/* <SQLQuery /> */}
     </TabsContent>
   );
 };
