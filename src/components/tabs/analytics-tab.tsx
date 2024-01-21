c4ff8ba44a7186c2229850b63d162cbe3fa5741a
Joey Yu <joey@jyu.dev>
2024-01-20 21:09:34 -0500

    chore: remove msgs

--- src/components/tabs/analytics-tab.tsx
+++ src/components/tabs/analytics-tab.tsx
@@ -1,27 +1,13 @@
 import React from "react";
 import { TabsContent } from "../ui/tabs";
 import SQLQuery from "../sql-query";
-import { type SelectMessage } from "~/types/message";
-import { Card } from "../ui/card";
 import Chatbot from "../chatbot";
 
-type Props = {
-  messages: SelectMessage[];
-};
-
-const AnalyticsTab = ({ messages }: Props) => {
+const AnalyticsTab = () => {
   return (
     <TabsContent value="analytics" className="space-y-4">
       <Chatbot />
       <SQLQuery />
-      {messages.map((message) => {
-        return (
-          <Card key={message.ID}>
-            {message.ID}
-            {message.MessageType}
-          </Card>
-        );
-      })}
     </TabsContent>
   );
 };
