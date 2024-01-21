7f56f3908a6c0e6d87effbc0e92f7ec60b40bf3f
Joey Yu <joey@jyu.dev>
2024-01-20 21:11:26 -0500

    chore: format

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -1,37 +1,32 @@
-'use client';
+"use client";
 
-import { unstable_noStore } from "next/cache";
-import { useChat } from 'ai/react';
-import DashboardPage from "~/components/dashboard";
-import { api } from "~/trpc/server";
+import { useChat } from "ai/react";
 
 export default function Chatbot() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
-    <div className="flex flex-col w-full max-w-md mx-auto border border-black">
-  <div className="overflow-auto py-4" style={{ minHeight: '300px' }}>
-    {messages.length === 0 ? (
-      <div className="text-left mx-4 text-gray-500">Ask a SQL query </div>
-    ) : (
-      messages.map((m) => (
-        <div key={m.id} className="whitespace-pre-wrap px-4 py-2">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
-        </div>
-      ))
-    )}
-  </div>
+    <div className="mx-auto flex w-full max-w-md flex-col border border-black">
+      <div className="overflow-auto py-4" style={{ minHeight: "300px" }}>
+        {messages.length === 0 ? (
+          <div className="mx-4 text-left text-gray-500">Ask a SQL query </div>
+        ) : (
+          messages.map((m) => (
+            <div key={m.id} className="whitespace-pre-wrap px-4 py-2">
+              {m.role === "user" ? "User: " : "AI: "}
+              {m.content}
+            </div>
+          ))
+        )}
+      </div>
 
-  <form onSubmit={handleSubmit} className="mt-auto w-full">
-    <input
-      className="w-full py-2 px-4 border-t border-black"
-      value={input}
-      placeholder="Say something..."
-      onChange={handleInputChange}
-    />
-  </form>
-</div>
-
-
+      <form onSubmit={handleSubmit} className="mt-auto w-full">
+        <input
+          className="w-full border-t border-black px-4 py-2"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
   );
 }
